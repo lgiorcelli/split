a224932e3111d95f6266327be18091578899dbd1
Leonardo Giorcelli <leonardo.giorcelli@etermax.com>
2019-05-10 00:03:44 -0300

    Refactor del index collector

--- src/main/java/com/lgior/split/index/IndexCollector.kt
+++ src/main/java/com/lgior/split/index/IndexCollector.kt
@@ -6,7 +6,6 @@ class IndexCollector {
 		if (thereIsNoMatch(input, toSearch)) {
 			return emptyList()
 		}
-//		return collectIndexOfMatches(input, toSearch)
 		return collectIndexOfMisses(input, toSearch)
 	}
 
@@ -16,29 +15,36 @@ class IndexCollector {
 		if (start > 0) {
 			index.add(Index(0, start))
 		}
-		while (start != -1) {
+		while (thereIsAMatch(start)) {
 			val nextMatch = input.indexOf(toSearch, start + toSearch.length)
-			if (nextMatch != -1) {
-				index.add(Index(start + toSearch.length, nextMatch))
-			} else {
-				if (start + toSearch.length != input.length) {
-					index.add(Index(start + toSearch.length, input.length))
-				}
+			val end = selectEndOfRange(nextMatch, input)
+			if (start + toSearch.length != input.length) {
+				index.add(Index(start + toSearch.length, end))
 			}
 			start = nextMatch
 		}
 		return index
 	}
 
+	private fun selectEndOfRange(nextMatch: Int, input: String): Int {
+		return if (thereIsAMatch(nextMatch)) {
+			nextMatch
+		} else {
+			input.length
+		}
+	}
+
 	private fun collectIndexOfMatches(input: String, toSearch: String): MutableList<Index> {
 		val index = mutableListOf<Index>()
 		var start = input.indexOf(toSearch)
-		while (start != -1) {
+		while (thereIsAMatch(start)) {
 			index.add(Index(start, start + toSearch.length))
 			start = input.indexOf(toSearch, start + toSearch.length)
 		}
 		return index
 	}
+
+	private fun thereIsAMatch(start: Int) = start != -1
 
 	private fun thereIsNoMatch(input: String, toSearch: String) = input.indexOf(toSearch) == -1
 }
